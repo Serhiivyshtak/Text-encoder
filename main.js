9f8f1bb89a4151dc9b0c8131a43eb45f221281f9
Serhiivyshtak <sergeyvishtak127@gmail.com>
2023-12-12 21:21:03 +0100

    1.4

--- main.js
+++ main.js
@@ -5,15 +5,21 @@ class Element {
     static decodeKeys = Object.fromEntries(Object.entries(Element.encodeKeys)
     .map((everyPair) => {return everyPair.reverse()}));
 ;
-    constructor (option) {
-        this.selector = option;
+    constructor (options) {
+        this.selector = options[0];
     }
 
     get HTMLElement () {
         return document.querySelector(this.selector)
     }
+}
 
-    static encode (string) {
+class Button extends Element {
+    constructor (options) {
+        super(options)
+    }
+
+    encode (string) {
         const encodedArray = [];
         for (let i = 0; i <= string.length; i++) {
             encodedArray.push(Element.encodeKeys[string[i]]);
@@ -22,7 +28,7 @@ class Element {
         encodedOutput.HTMLElement.innerText = encodedString;
     } 
 
-    static decode (string) {
+    decode (string) {
         const decodedArray = [];
         for (let i = 0; i <= string.length; i++) {
             decodedArray.push(Element.decodeKeys[string[i]]);
@@ -31,15 +37,15 @@ class Element {
         decodedOutput.HTMLElement.innerText = decodedString;
     }
 
-    static clearEncodeInput () {
+    clearEncodeInput () {
         encodeInput.HTMLElement.value = '';
     }
 
-    static clearDecodeInput () {
+    clearDecodeInput () {
         decodeInput.HTMLElement.value = '';
     }
 
-    static copyEncodedOutput () {
+    copyEncodedOutput () {
         if (encodedOutput.HTMLElement.value.trim() == '') {
             copyEncodedButton.HTMLElement.innerText = 'output is empty';
             copyEncodedButton.HTMLElement.classList.remove('btn-outline-secondary');
@@ -62,7 +68,7 @@ class Element {
         }
     }
 
-    static copyDecodedOutput () {
+    copyDecodedOutput () {
         if (decodedOutput.HTMLElement.value.trim() == '') {
             copyDecodedButton.HTMLElement.innerText = 'output is empty';
             copyDecodedButton.HTMLElement.classList.remove('btn-outline-secondary');
@@ -84,43 +90,83 @@ class Element {
             }, 3000)
         }
     }
+
+    changeTheme () {
+        const blocksToChangeColorTheme = document.querySelectorAll('.block_to_change_color');
+        const textToCgangeColorTheme = document.querySelectorAll('.text_to_change_color');
+        for (let everyBlock of blocksToChangeColorTheme) {
+            everyBlock.classList.toggle('bg-dark');
+            everyBlock.classList.toggle('bg-light');
+        }
+        for (let everyText of textToCgangeColorTheme) {
+            everyText.classList.toggle('text-dark');
+            everyText.classList.toggle('text-white');
+        }
+        themeToggleButton.HTMLElement.children[0].children[0].classList.toggle('fill_dark');
+        themeToggleButton.HTMLElement.children[0].children[0].classList.toggle('fill_light');
+    }
 }
 
+class Select extends Element {
+    constructor (options) {
+        super(options)
+    }
 
+    changeLanguage () {
+        let selectValue = Number(selectLanguage.HTMLElement.value);
+        if (selectValue === 1) {
+            console.log('EN');
+        } if (selectValue === 2) {
+            console.log('DE');
+        } if (selectValue === 3) {
+            console.log('UA');
+        }
+    }
+}
 
-let encodeButton = new Element ('#encode_button');
-let decodeButton = new Element ('#decode_button');
-let encodeInput = new Element ('#encode_text_area');
-let decodeInput = new Element ('#decode_text_area');
-let encodedOutput = new Element ('#encoded_output_container');
-let decodedOutput = new Element ('#decoded_output_container');
-let clearEncodeInputButton = new Element ('#clear_encode_input_button');
-let clearDecodeInputButton = new Element ('#clear_decode_input_button');
-let copyEncodedButton = new Element ('#encode_copy_button');
-let copyDecodedButton = new Element ('#decode_copy_button');
+let encodeInput = new Element (['#encode_text_area']);
+let decodeInput = new Element (['#decode_text_area']);
+let encodedOutput = new Element (['#encoded_output_container']);
+let decodedOutput = new Element (['#decoded_output_container']);
+
+let themeToggleButton = new Button(['#theme_toggle_button']);
+let encodeButton = new Button (['#encode_button']);
+let decodeButton = new Button (['#decode_button']);
+let clearEncodeInputButton = new Button (['#clear_encode_input_button']);
+let clearDecodeInputButton = new Button (['#clear_decode_input_button']);
+let copyEncodedButton = new Button (['#encode_copy_button']);
+let copyDecodedButton = new Button (['#decode_copy_button']);
+
+let selectLanguage = new Select(['#language_select']);
 
 
 document.addEventListener('click', (e) => {
-    if (e.target == encodeButton.HTMLElement) {
-        Element.encode(encodeInput.HTMLElement.value.trim());
+    if (e.target === encodeButton.HTMLElement) {
+        encodeButton.encode(encodeInput.HTMLElement.value.trim());
     }
-    if (e.target == clearEncodeInputButton.HTMLElement) {
-        Element.clearEncodeInput();
+    if (e.target === clearEncodeInputButton.HTMLElement) {
+        clearEncodeInputButton.clearEncodeInput();
     }
-    if (e.target == decodeButton.HTMLElement) {
-        Element.decode(decodeInput.HTMLElement.value.trim())
+    if (e.target === decodeButton.HTMLElement) {
+        decodeButton.decode(decodeInput.HTMLElement.value.trim())
     }
-    if (e.target == clearDecodeInputButton.HTMLElement) {
-        Element.clearDecodeInput();
+    if (e.target === clearDecodeInputButton.HTMLElement) {
+        clearDecodeInputButton.clearDecodeInput();
     }
-    if (e.target == copyEncodedButton.HTMLElement) {
-        Element.copyEncodedOutput();
+    if (e.target === copyEncodedButton.HTMLElement) {
+        copyEncodedButton.copyEncodedOutput();
     } 
-    if (e.target == copyDecodedButton.HTMLElement) {
-        Element.copyDecodedOutput();
+    if (e.target === copyDecodedButton.HTMLElement) {
+        copyDecodedButton.copyDecodedOutput();
     }
-})
+    if (e.target === themeToggleButton.HTMLElement || e.target === themeToggleButton.HTMLElement.children[0] || e.target === themeToggleButton.HTMLElement.children[0].children[0]) {
+        themeToggleButton.changeTheme();
+    }
+});
 
+selectLanguage.HTMLElement.addEventListener('change', () => {
+    selectLanguage.changeLanguage();
+});
 
 
 // What to improve:
@@ -129,3 +175,4 @@ document.addEventListener('click', (e) => {
 // 3) Make optimization of code; 
 // 4) Make header with ability to change the theme and language
 // 5) Make footer with
+
